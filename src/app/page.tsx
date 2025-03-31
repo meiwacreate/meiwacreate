ecdb7a4fa182fba28a4ce21afa17f03d4131bf86
meiwacreate <meiwacreate@gmail.com>
2025-03-31 15:54:32 +0900

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,26 @@ import { FaGithub } from "react-icons/fa"
 
 export default function Home() {
   return (
-    <div className="md:max-w-md w-full mx-auto px-4 md:px-0 py-8">
-      <Image src="/avatar.jpg" alt="Avatar" width={100} height={100} className="rounded-full" />
-      <h1 className="font-semibold text-xl mt-2">meiwa</h1>
-      <div className="flex flex-wrap gap-2 mt-2 text-sm">
-        <Link href="https://github.com/meiwacreate" target="_blank" rel="noopener noreferrer" passHref><p className="px-2.5 py-0.5 bg-gray-50 border text-gray-600 rounded-full flex items-center"><FaGithub className="mr-1.5" />GitHub</p></Link>
-        <Link href="https://zenn.dev/meiwacreate" target="_blank" rel="noopener noreferrer" passHref><p className="px-2.5 py-0.5 bg-gray-50 border text-gray-600 rounded-full flex items-center"><Image src="/zenn-logo.svg" alt="Zenn Logo" width={100} height={100} className="w-4 mr-1.5" />Zenn</p></Link>
-      </div>
-      <p className="mt-2 text-gray-600">I am a student. I mainly focus on front-end development and build things that I want to create.</p>
-      <div className="flex flex-wrap gap-2 mt-2">
-        <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">HTML</p>
-        <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">CSS</p>
-        <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">JavaScript</p>
-        <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">TypeScript</p>
-        <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">Node.js</p>
-        <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">React</p>
-        <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">Next.js</p>
-        <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">Ruby</p>
-        <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">Ruby on Rails</p>
+    <div className="px-4 md:px-0 py-8 min-h-screen bg-gradient-to-br from-blue-800 via-indigo-800 to-violet-800">
+      <div className="border border-white rounded-lg bg-white/75 backdrop-blur-3xl p-4 md:max-w-md w-full mx-auto shadow-lg">
+        <Image src="/avatar.jpg" alt="Avatar" width={100} height={100} className="rounded-full" />
+        <h1 className="font-semibold text-xl mt-2">meiwa</h1>
+        <div className="flex flex-wrap gap-2 mt-2 text-sm">
+          <Link href="https://github.com/meiwacreate" target="_blank" rel="noopener noreferrer" passHref><p className="px-2.5 py-0.5 bg-gray-50 border text-gray-600 rounded-full flex items-center"><FaGithub className="mr-1.5" />GitHub</p></Link>
+          <Link href="https://zenn.dev/meiwacreate" target="_blank" rel="noopener noreferrer" passHref><p className="px-2.5 py-0.5 bg-gray-50 border text-gray-600 rounded-full flex items-center"><Image src="/zenn-logo.svg" alt="Zenn Logo" width={100} height={100} className="w-4 mr-1.5" />Zenn</p></Link>
+        </div>
+        <p className="mt-2 text-gray-600">I am a student. I mainly focus on front-end development and build things that I want to create.</p>
+        <div className="flex flex-wrap gap-2 mt-2">
+          <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">HTML</p>
+          <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">CSS</p>
+          <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">JavaScript</p>
+          <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">TypeScript</p>
+          <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">Node.js</p>
+          <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">React</p>
+          <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">Next.js</p>
+          <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">Ruby</p>
+          <p className="px-2 py-1 bg-gray-200 text-gray-600 rounded-md text-sm">Ruby on Rails</p>
+        </div>
       </div>
     </div>
   )
